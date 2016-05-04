e7dc59946cf3b8728b2b50d14c805a6aebb8f8e9
ankihg <hagelin.annika@gmail.com>
2016-05-04 13:36:03 -0700

    project delete e2e test

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -12,7 +12,7 @@ describe('end to end testing', function() {
       element(by.buttonText('sign in')).click()
         .then(function() {
           //nav to project-crud
-          browser.get(clientServer+'/#/project-crud');
+          // browser.get(clientServer+'/#/project-crud');
         });
     });
 
@@ -35,71 +35,32 @@ describe('end to end testing', function() {
     //     })
     // });
 
-    describe('created, update, delete project', function() {
+    describe('created, update, delete a project', function() {
 
       beforeEach(function() {
-        //sign in
-        browser.get(clientServer+'/#/signin');
-        element(by.model('user.username')).clear().sendKeys('tad');
-        element(by.model('user.password')).clear().sendKeys('salem');
-        element(by.buttonText('sign in')).click()
+        //create a project
+        var createForm = $('#project-crud').$('#create-project');
+        element(by.model('project.name')).clear().sendKeys('plz test');
+        element(by.model('project.url')).clear().sendKeys('http://plz.com');
+        element(by.model('project.imgSrc')).clear().sendKeys('https://media.giphy.com/media/CL5FfrZoVR3Gw/giphy.gif');
+        element(by.model('project.date')).clear().sendKeys('5/4/16');
+        element(by.model('project.tags')).clear().sendKeys('plz,respond');
+        element(by.model('project.about')).clear().sendKeys('<p>this is a test</p>');
+        element(by.buttonText('create')).click()
           .then(function() {
-            //nav to project-crud
-            browser.get(clientServer+'/#/project-crud');
+            element.all(by.repeater('project in crudCtrl.projects').column('name')).getText()
+              .then(function(names) {
+                var newName = names[names.length-1];
+                expect(newName).toBe('plz test');
 
-            //create a project
-            var createForm = $('#project-crud').$('#create-project');
-            element(by.model('project.name')).clear().sendKeys('plz');
-            element(by.model('project.url')).clear().sendKeys('http://plz.com');
-            element(by.model('project.imgSrc')).clear().sendKeys('https://media.giphy.com/media/CL5FfrZoVR3Gw/giphy.gif');
-            element(by.model('project.date')).clear().sendKeys('5/4/16');
-            element(by.model('project.tags')).clear().sendKeys('plz,respond');
-            element(by.model('project.about')).clear().sendKeys('<p>this is a test</p>');
-            element(by.buttonText('create')).click()
-              .then(function() {
-                console.log('button click');
-                element.all(by.repeater('project in crudCtrl.projects').column('name')).getText()
-                  .then(function(names) {
-                    console.log(names);
-                    var newName = names[names.length-1];
-                    console.log(newName);
-
-                    browser.manage().logs().get('browser').then(function(browserLogs) {
-                       // browserLogs is an array of objects with level and message fields
-                       browserLogs.forEach(function(log){
-                          console.log(log.message);
-                        });
-                    });
-
-                  });
+                // browser.manage().logs().get('browser').then(function(browserLogs) {
+                //    browserLogs.forEach(function(log){
+                //       console.log(log.message);
+                //     });
+                // });
               });
           });
-        // //create a project
-        // var createForm = $('#project-crud').$('#create-project');
-        // element(by.model('project.name')).clear().sendKeys('plz');
-        // element(by.model('project.url')).clear().sendKeys('http://plz.com');
-        // element(by.model('project.imgSrc')).clear().sendKeys('https://media.giphy.com/media/CL5FfrZoVR3Gw/giphy.gif');
-        // element(by.model('project.date')).clear().sendKeys('5/4/16');
-        // element(by.model('project.tags')).clear().sendKeys('plz,respond');
-        // element(by.model('project.about')).clear().sendKeys('<p>this is a test</p>');
-        // element(by.buttonText('create')).click()
-        //   .then(function() {
-        //     console.log('button click');
-        //     element.all(by.repeater('project in crudCtrl.projects').column('name')).getText()
-        //       .then(function(names) {
-        //         console.log(names);
-        //         var newName = names[names.length-1];
-        //         console.log(newName);
-        //
-        //         browser.manage().logs().get('browser').then(function(browserLogs) {
-        //            // browserLogs is an array of objects with level and message fields
-        //            browserLogs.forEach(function(log){
-        //               console.log(log.message);
-        //             });
-        //         });
-        //
-        //       });
-        //   });
+
 
       });
 
@@ -107,6 +68,25 @@ describe('end to end testing', function() {
         console.log('will update');
       });
 
+      afterEach(function() {
+        element.all(by.repeater('project in crudCtrl.projects'))
+          .then(function(projects) {
+            var nProjects = projects.length;
+            var testProj = projects[projects.length-1];
+            testProj.element(by.buttonText('delete')).click()
+              .then(function() {
+                element.all(by.repeater('project in crudCtrl.projects'))
+                  .then(function(projects) {
+                    expect(projects.length).toBe(nProjects-1);
+                  });
+              });
+
+            // .getText()
+            //   .then(function(testProj) {
+            //     console.log(testProj);
+            //   });
+          })
+      });
 
 
     });
