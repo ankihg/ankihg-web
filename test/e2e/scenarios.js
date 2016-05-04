59f04b0483139df861467afd40cb3fff3a10101d
ankihg <hagelin.annika@gmail.com>
2016-05-04 09:55:12 -0700

    some init e2e tests

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -2,12 +2,38 @@ describe('end to end testing', function() {
 
   var clientServer = require('../../config').clientServerUrl;
 
-  beforeEach(function() {
-    browser.get(clientServer);
+  describe('project crud testing', function() {
+
+    beforeEach(function() {
+      browser.get(clientServer+'/#/project-crud');
+    });
+
+    it('have project element for each project', function() {
+      element.all(by.repeater('project in crudCtrl.projects'))
+        .then(function(projects) {
+          element(by.repeater('project in crudCtrl.projects')).evaluate('crudCtrl.projects.length')
+            .then(function(nProjects) {
+              expect(projects.length).toBe(nProjects);
+            });
+        })
+    });
+
+    it('projects have name', function() {
+      element.all(by.repeater('project in crudCtrl.projects').column('name')).getText()
+        .then(function(names) {
+          names.forEach(function(name) {
+            expect(name).toBeDefined();
+          });
+        })
+    });
+
+    // it('created, update, delete project', function() {
+    //
+    //   
+    // })
+
+
   });
 
-  it('false be true', function() {
-    expect(true).toBe(false);
-  })
 
 })
