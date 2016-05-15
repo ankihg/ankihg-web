7aca372ff53dea3528a3fae0601e0ffab848812b
ankihg <hagelin.annika@gmail.com>
2016-05-15 11:15:01 -0700

    squashed server require config

--- squashed-server.js
+++ squashed-server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const PORT = config.PORT;
+const PORT = require('./config').PORT;
 
 //server
 const app = new express();
