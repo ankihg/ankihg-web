7e62f300319d512d8acb6465fb23da9abd928f68
ankihg <hagelin.annika@gmail.com>
2016-05-15 11:11:49 -0700

    squashed server created and config altered

--- config.js
+++ config.js
@@ -1,6 +1,14 @@
 exports.DB_PORT = process.env.MY_DB_URI || 'mongodb://localhost/db';
-exports.serverPort = 3000;
-exports.clientServerPort = process.env.PORT || 8080;
+// exports.serverPort = 3000;
+// exports.clientServerPort = process.env.PORT || 8080;
+
+// exports.serverUrl = 'http://localhost:'+exports.serverPort;
+// exports.clientServerUrl = 'http://localhost:'+exports.clientServerPort;
+
+//squashed server
+exports.PORT = process.env.PORT:
+exports.serverPort = exports.PORT;
+exports.clientServerPort = exports.PORT;
 
 exports.serverUrl = 'http://localhost:'+exports.serverPort;
 exports.clientServerUrl = 'http://localhost:'+exports.clientServerPort;
---
+++ squashed-server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const PORT = config.PORT;
+
+//server
+const app = new express();
+const router = new express.Router();
+
+const models = require('./models');
+
+app.use(bodyParser.json());
+
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', require('./config').clientServerUrl);
+//   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, token');
+//   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+//   next();
+// });
+
+app.use((req, res, next) => {
+  console.log(`${req.method} request for ${req.url}`);
+  next();
+})
+
+require('./routes')(router, models);
+
+app.use(router);
+
+
+
+//client
+app
+  .use((req, res, next) => {console.log(`${req.method} request for ${req.url}`); next(); })
+  .use(express.static('./build'));
+
+
+app.listen(PORT, () => console.log('server speaking on '+PORT));
